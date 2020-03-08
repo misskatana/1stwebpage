37ae650a54d7905d0d3d4019e3f8a2e0475cd78a
Katana <sdevkatana@gmail.com>
2020-03-08 18:59:38 -0500

    first web page

---
+++ app.js
@@ -0,0 +1,25 @@
+// this is comment tag
+var pics = [
+    "Backgrounds/Doggo.png",
+    "Backgrounds/Ducky.png",
+    "Backgrounds/Empire.jpg",
+    "Backgrounds/Garden.png",
+    "Backgrounds/Home.png",
+    "Backgrounds/Hubby.png",
+    "Backgrounds/Kids.png",
+    "Backgrounds/Library nook.png",
+    "Backgrounds/Rig.png",
+    "Backgrounds/Standy.png"
+];
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+
+btn.addEventListener("click", function() {
+    if (counter === 10) {
+        counter = 0;
+    }
+    img.src = pics[counter]
+    counter = counter + 1;
+});
